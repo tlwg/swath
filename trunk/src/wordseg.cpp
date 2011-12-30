fb2da7067e053d14bd06af9c6ca8e3523fc3409f
thep-svn <thep-svn@5165837f-0e3e-446d-8197-18d607f76d43>
2011-12-30 13:00:29 +0000

    * src/wordseg.cpp: 	  - Apply "indent -nut" + manual adjustments.

--- trunk/src/wordseg.cpp
+++ trunk/src/wordseg.cpp
@@ -5,13 +5,13 @@
 // 1999
 // ==========================================
 // New features.
-//	 1. Support many formats: LaTex, HTML,
-//		 Text, RTF (for Opensource, Eamac 1999)
-//	 2. Remove garbage characters.
-//		 (for a. virach requirement 2000)
-//	 3. Select wordseg algorithms, Bi-gram
-//	     Maximal Matching, Longest Matching
-//		 (for Opensource, Eamac 1999)
+//       1. Support many formats: LaTeX, HTML,
+//               Text, RTF (for Opensource, Emacs 1999)
+//       2. Remove garbage characters.
+//               (for a. virach requirement 2000)
+//       3. Select wordseg algorithms, Bi-gram
+//           Maximal Matching, Longest Matching
+//               (for Opensource, Emacs 1999)
 // ===========================================
 
 
@@ -37,46 +37,53 @@
 // 5: not found dtail.tri
 // 6: not found ptail.tri
 
-int myisspace(int ch);
-int SplitToken(char **str, char *token);
-bool IsJunkChar(unsigned char ch);
-void RemoveJunkChar(char *str);
+int myisspace (int ch);
+int SplitToken (char **str, char *token);
+bool IsJunkChar (unsigned char ch);
+void RemoveJunkChar (char *str);
 
 
 
-int InitWordSegmentation(const char *dictpath,
-                         const char *method,
-                         AbsWordSeg **pWseg)
+int
+InitWordSegmentation (const char *dictpath,
+                      const char *method, AbsWordSeg ** pWseg)
 {
-  if (method==NULL){
-    *pWseg=new MaxWordSeg;
-  } else if (strcmp(method,"long")==0){
-    *pWseg=new LongWordSeg;
-  }else{
-    *pWseg=new MaxWordSeg;
-  }
+  if (method == NULL)
+    {
+      *pWseg = new MaxWordSeg;
+    }
+  else if (strcmp (method, "long") == 0)
+    {
+      *pWseg = new LongWordSeg;
+    }
+  else
+    {
+      *pWseg = new MaxWordSeg;
+    }
 
   // Dict search order:
   // 1. dictpath, if not NULL, exit on failure
   // 2. ${SWATHDICT} env, if not NULL, continue on failure
   // 3. current dir, continue on failure
   // 4. WORDSEGDATADIR macro
-  if (dictpath) {
-    if (!(*pWseg)->InitDict(dictpath)) {
-      delete *pWseg;
-      return 1;
+  if (dictpath)
+    {
+      if (!(*pWseg)->InitDict (dictpath))
+        {
+          delete *pWseg;
+          return 1;
+        }
+      return 0;
     }
-    return 0;
-  }
 
-  dictpath = getenv("SWATHDICT");
-  if (dictpath && (*pWseg)->InitDict(dictpath))
+  dictpath = getenv ("SWATHDICT");
+  if (dictpath && (*pWseg)->InitDict (dictpath))
     return 0;
 
-  if ((*pWseg)->InitDict("."))
+  if ((*pWseg)->InitDict ("."))
     return 0;
 
-  if ((*pWseg)->InitDict(WORDSEGDATA_DIR))
+  if ((*pWseg)->InitDict (WORDSEGDATA_DIR))
     return 0;
 
   // All fail
@@ -84,59 +91,63 @@ int InitWordSegmentation(const char *dictpath,
   return 1;
 }
 
-void ExitWordSegmentation(AbsWordSeg *wseg)
+void
+ExitWordSegmentation (AbsWordSeg * wseg)
 {
   delete wseg;
 }
 
-void WordSegmentation(AbsWordSeg *wseg, const char *wbr, char *line,
-                      char* output)
+void
+WordSegmentation (AbsWordSeg * wseg, const char *wbr, char *line,
+                  char *output)
 {
-	//RemoveJunkChar(line);
-	wseg->WordSeg(line,output,wbr);
+  //RemoveJunkChar(line);
+  wseg->WordSeg (line, output, wbr);
 }
 
-static void Version()
+static void
+Version ()
 {
-    printf("swath " VERSION "\n");
-    printf(
-"Copyright (C) 2001-2006 Phaisarn Charoenpornsawat <phaisarn@nectec.or.th>\n"
-"Copyright (C) 2001-2009 Theppitak Karoonboonyanan <thep@linux.thai.net>\n"
-"License: GNU GPL version 2 or later <http://gnu.org/licenses/gpl-2.0.html>\n"
-"This is free software; you are free to change and redistribute it.\n"
-"There is NO WARRANTY, to the extent permitted by law.\n"
-    );
+  printf ("swath " VERSION "\n");
+  printf
+    ("Copyright (C) 2001-2006 Phaisarn Charoenpornsawat <phaisarn@nectec.or.th>\n"
+     "Copyright (C) 2001-2009 Theppitak Karoonboonyanan <thep@linux.thai.net>\n"
+     "License: GNU GPL version 2 or later <http://gnu.org/licenses/gpl-2.0.html>\n"
+     "This is free software; you are free to change and redistribute it.\n"
+     "There is NO WARRANTY, to the extent permitted by law.\n");
 }
 
-static void Usage(int verbose)
+static void
+Usage (int verbose)
 {
-    fprintf(stderr,
-            "Usage: swath [mule|-v] [-b \"delimitor\"] [-d dict-dir]\n"
-            "[-f html|rtf|latex|lambda] [-m long|max] [-u {u|t},{u|t}] [-help]\n");
-    if (verbose) {
-	  fprintf(stderr,
-	          "Options:\n"
-	          "\tmule : for use with mule\n"
-	          "\t-v   : verbose mode\n"
-	          "\t-b   : define a word delimitor string for the output\n"
-	          "\t-d   : specify dictionary path\n"
-	          "\t-f   : specify format of the input\n"
-	          "\t\thtml     : HTML file\n"
-	          "\t\trtf      : RTF file\n"
-	          "\t\tlatex    : LaTeX file\n"
-	          "\t\tlambda   : The input and output are same as latex, except that\n"
-	          "\t\t           the word delimitor is ^^^^^^^^200c\n"
-//	          "\t\twinlatex : LaTeX file shaping on Windows\n"
-//	          "\t\tmaclatex : LaTeX file shaping on Macintosh\n"
-	          "\t-m   : choose word matching scheme when analyzing\n"
-	          "\t\tlong     : longest matching scheme\n"
-	          "\t\tmax      : maximal matching scheme\n"
-//	          "\t-l   : line processing(effect only in a bigram algo.)\n"
-	          "\t-u   : specify encodings of input and output in 'i,o' form,\n"
-	          "\t       for input and output respectively, where 'i', 'o' is one of:\n"
-		  "\t\tu        : The input/output is in UTF-8\n"
-		  "\t\tt        : The input/output is in TIS-620\n"
-	          "\t-help: display this help message\n");
+  fprintf (stderr,
+           "Usage: swath [mule|-v] [-b \"delimitor\"] [-d dict-dir]\n"
+           "[-f html|rtf|latex|lambda] [-m long|max] [-u {u|t},{u|t}] [-help]\n");
+  if (verbose)
+    {
+      fprintf (stderr,
+               "Options:\n"
+               "\tmule : for use with mule\n"
+               "\t-v   : verbose mode\n"
+               "\t-b   : define a word delimitor string for the output\n"
+               "\t-d   : specify dictionary path\n"
+               "\t-f   : specify format of the input\n"
+               "\t\thtml     : HTML file\n"
+               "\t\trtf      : RTF file\n"
+               "\t\tlatex    : LaTeX file\n"
+               "\t\tlambda   : The input and output are same as latex, except that\n"
+               "\t\t           the word delimitor is ^^^^^^^^200c\n"
+//                "\t\twinlatex : LaTeX file shaping on Windows\n"
+//                "\t\tmaclatex : LaTeX file shaping on Macintosh\n"
+               "\t-m   : choose word matching scheme when analyzing\n"
+               "\t\tlong     : longest matching scheme\n"
+               "\t\tmax      : maximal matching scheme\n"
+//                "\t-l   : line processing(effect only in a bigram algo.)\n"
+               "\t-u   : specify encodings of input and output in 'i,o' form,\n"
+               "\t       for input and output respectively, where 'i', 'o' is one of:\n"
+               "\t\tu        : The input/output is in UTF-8\n"
+               "\t\tt        : The input/output is in TIS-620\n"
+               "\t-help: display this help message\n");
     }
 }
 
@@ -147,213 +158,278 @@ static void Usage(int verbose)
 
 #define MAXCHAR 2000
 
-int main(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-  char mode = 1;  // 0 = display, 1 = don't display message
+  char mode = 1;                // 0 = display, 1 = don't display message
   const char *wbr;
   const char *dictpath = NULL;
-  const char *method=NULL;
+  const char *method = NULL;
   const char *fileformat = NULL;
   const char *unicode = NULL;
   bool muleMode;
   bool thaifag;
-  bool wholeLine=false;
-  
+  bool wholeLine = false;
+
   wbr = "|";
   muleMode = false;
-  for (int iargc = 1; iargc < argc; iargc++) {
-    if (strcmp("mule", argv[iargc]) == 0){
-      muleMode = true;
-      mode = 1;
-    } else if (strcmp("-b", argv[iargc]) == 0 && iargc + 1 < argc) {
-      wbr = argv[++iargc];
-    } else if (strcmp("-d", argv[iargc]) == 0 && iargc + 1 < argc) {
-      dictpath = argv[++iargc];
-    } else if (strcmp("-f", argv[iargc]) == 0 && iargc + 1 < argc) {
-      fileformat = argv[++iargc];
-    } else if (strcmp("-v", argv[iargc]) == 0 ||
-               strcmp("--verbose", argv[iargc]) == 0)
+  for (int iargc = 1; iargc < argc; iargc++)
     {
-      mode=0;
-    } else if (strcmp("-m", argv[iargc]) == 0 && iargc + 1 < argc) {
-      method = argv[++iargc];
-    } else if (strcmp("-l", argv[iargc]) == 0) {
-      //send only token which has no white space in to wordseg
-      wholeLine=true;
-    } else if (strcmp("-u", argv[iargc]) ==0 && iargc + 1 < argc) {
-      unicode = argv[++iargc];
-    } else if (strcmp("-V", argv[iargc]) == 0 ||
-               strcmp("--version", argv[iargc]) == 0)
-    {
-      Version();
-      return 0;
-    } else if (strcmp("-help", argv[iargc]) == 0 ||
-               strcmp("--help", argv[iargc]) == 0)
-    {
-      Usage(1);
-      return 1;
-    } else {
-      Usage(0);
-      return 1;
+      if (strcmp ("mule", argv[iargc]) == 0)
+        {
+          muleMode = true;
+          mode = 1;
+        }
+      else if (strcmp ("-b", argv[iargc]) == 0 && iargc + 1 < argc)
+        {
+          wbr = argv[++iargc];
+        }
+      else if (strcmp ("-d", argv[iargc]) == 0 && iargc + 1 < argc)
+        {
+          dictpath = argv[++iargc];
+        }
+      else if (strcmp ("-f", argv[iargc]) == 0 && iargc + 1 < argc)
+        {
+          fileformat = argv[++iargc];
+        }
+      else if (strcmp ("-v", argv[iargc]) == 0 ||
+               strcmp ("--verbose", argv[iargc]) == 0)
+        {
+          mode = 0;
+        }
+      else if (strcmp ("-m", argv[iargc]) == 0 && iargc + 1 < argc)
+        {
+          method = argv[++iargc];
+        }
+      else if (strcmp ("-l", argv[iargc]) == 0)
+        {
+          //send only token which has no white space in to wordseg
+          wholeLine = true;
+        }
+      else if (strcmp ("-u", argv[iargc]) == 0 && iargc + 1 < argc)
+        {
+          unicode = argv[++iargc];
+        }
+      else if (strcmp ("-V", argv[iargc]) == 0 ||
+               strcmp ("--version", argv[iargc]) == 0)
+        {
+          Version ();
+          return 0;
+        }
+      else if (strcmp ("-help", argv[iargc]) == 0 ||
+               strcmp ("--help", argv[iargc]) == 0)
+        {
+          Usage (1);
+          return 1;
+        }
+      else
+        {
+          Usage (0);
+          return 1;
+        }
     }
-  }
 
-  if (mode == 0) printf("*** Word Segmentation ***\n");
+  if (mode == 0)
+    printf ("*** Word Segmentation ***\n");
 
   AbsWordSeg *wseg;
-  
-  char line[MAXCHAR+1], output[MAXCHAR*2+1];
+
+  char line[MAXCHAR + 1], output[MAXCHAR * 2 + 1];
   int i;
   int c;
-  char leadch[3],folch[3];
+  char leadch[3], folch[3];
 
-  if (InitWordSegmentation(dictpath, method, &wseg) != 0)
+  if (InitWordSegmentation (dictpath, method, &wseg) != 0)
     return 1;
 
-  leadch[0]='\0';
-  folch[0]='\0';
+  leadch[0] = '\0';
+  folch[0] = '\0';
 
 
 
-  FILE *tmpout=stdout, *tmpin=stdin;
-  if (unicode!=NULL) { //Option -u
-	  if (unicode[0]=='u') { //unicode input file.
-		  tmpin = tmpfile();
-		  conv('t', NULL, tmpin);
-		  rewind(tmpin);
-	  }
-	  if (unicode[2]=='u'){
-		  tmpout = tmpfile();
-	  }
-  }
-  if (fileformat!=NULL) {
-	  FilterX* FltX = FileFilter::CreateFilter(tmpin,tmpout,fileformat);
-	  if (FltX==NULL) {
-		fprintf(stderr, "Invalid file format: %s\n", fileformat);
-		// FIXME: still mem leak hmm..
-		return 1;
-	  }
-	  wbr = FltX->GetWordBreak();
-	  while (FltX->GetNextToken(line,&thaifag)){
-		if (!thaifag){
-			FltX->Print(line,thaifag);
-			continue;
-		}
-		WordSegmentation(wseg, wbr, line, output);
-		FltX->Print(output,thaifag);
-	  }
-	  delete FltX;
-  }else{
-	  char stopstr[20];
-	  if (muleMode)
-		strcpy(stopstr,wbr);
-	  else
-		stopstr[0]='\0';
-	  for (;;) { // read until end of file.
-		if (mode == 0) printf("Input : ");
-		for (i = 0; ((c = fgetc(tmpin)) != '\n')
-			&& (i <= MAXCHAR) && (c!=EOF);i++) 
-			line[i] = (char)c;
-		line[i] = 0;
-		//if (strcmp(line, "exit") == 0) break;
-		if (line[0]=='\0') 
-			if (c==EOF) break; else continue;
-		int tokenFlag;
-		char *startStr=line;
-		char buff[2000],gout[5000];
-		gout[0] = '\0';
-		if (!wholeLine) {
- 			while ((tokenFlag=SplitToken(&startStr,buff))>=0) {
-				if (tokenFlag==-1)
-					break;
-				if (tokenFlag==0) {
-					strcat(gout,buff);
-				} else {
-					WordSegmentation(wseg, wbr, buff, output);
-					strcat(gout,output);
-				}
-				strcat(gout, wbr);
-			}
-		}else{
-			WordSegmentation(wseg, wbr, line, output);
-			strcpy(gout,output);
-			strcat(gout,stopstr);
-		}
-		if (mode == 0) printf("Output: ");
-		fprintf(tmpout, "%s\n", gout);
-		if (feof(tmpin)) break;
-	  }// end for (;;)
-  }
-  if (unicode!=NULL) {
-	  if (unicode[2]=='u') {
-		rewind(tmpout);
-		conv('u', tmpout, NULL);
-		fclose(tmpout);
-	  }
-	  if (unicode[0]=='u') {
-		fclose(tmpin);
-	  }
-  }
+  FILE *tmpout = stdout, *tmpin = stdin;
+  if (unicode != NULL)
+    {                           //Option -u
+      if (unicode[0] == 'u')
+        {                       //unicode input file.
+          tmpin = tmpfile ();
+          conv ('t', NULL, tmpin);
+          rewind (tmpin);
+        }
+      if (unicode[2] == 'u')
+        {
+          tmpout = tmpfile ();
+        }
+    }
+  if (fileformat != NULL)
+    {
+      FilterX *FltX = FileFilter::CreateFilter (tmpin, tmpout, fileformat);
+      if (FltX == NULL)
+        {
+          fprintf (stderr, "Invalid file format: %s\n", fileformat);
+          // FIXME: still mem leak hmm..
+          return 1;
+        }
+      wbr = FltX->GetWordBreak ();
+      while (FltX->GetNextToken (line, &thaifag))
+        {
+          if (!thaifag)
+            {
+              FltX->Print (line, thaifag);
+              continue;
+            }
+          WordSegmentation (wseg, wbr, line, output);
+          FltX->Print (output, thaifag);
+        }
+      delete FltX;
+    }
+  else
+    {
+      char stopstr[20];
+      if (muleMode)
+        strcpy (stopstr, wbr);
+      else
+        stopstr[0] = '\0';
+      for (;;)
+        {                       // read until end of file.
+          if (mode == 0)
+            printf ("Input : ");
+          for (i = 0; (c = fgetc (tmpin)) != '\n' && c != EOF
+                      && i <= MAXCHAR; i++)
+            {
+              line[i] = (char) c;
+            }
+          line[i] = 0;
+          //if (strcmp(line, "exit") == 0) break;
+          if (line[0] == '\0')
+            {
+              if (c == EOF)
+                break;
+              continue;
+            }
+          int tokenFlag;
+          char *startStr = line;
+          char buff[2000], gout[5000];
+          gout[0] = '\0';
+          if (!wholeLine)
+            {
+              while ((tokenFlag = SplitToken (&startStr, buff)) >= 0)
+                {
+                  if (tokenFlag == -1)
+                    break;
+                  if (tokenFlag == 0)
+                    {
+                      strcat (gout, buff);
+                    }
+                  else
+                    {
+                      WordSegmentation (wseg, wbr, buff, output);
+                      strcat (gout, output);
+                    }
+                  strcat (gout, wbr);
+                }
+            }
+          else
+            {
+              WordSegmentation (wseg, wbr, line, output);
+              strcpy (gout, output);
+              strcat (gout, stopstr);
+            }
+          if (mode == 0)
+            printf ("Output: ");
+          fprintf (tmpout, "%s\n", gout);
+          if (feof (tmpin))
+            break;
+        }                       // end for (;;)
+    }
+  if (unicode != NULL)
+    {
+      if (unicode[2] == 'u')
+        {
+          rewind (tmpout);
+          conv ('u', tmpout, NULL);
+          fclose (tmpout);
+        }
+      if (unicode[0] == 'u')
+        {
+          fclose (tmpin);
+        }
+    }
 
-  ExitWordSegmentation(wseg);
+  ExitWordSegmentation (wseg);
 
   return 0;
 }
 
-int SplitToken(char **str, char *token){
 //return -1 in case there is no data in str
 //return 0 for a token which is contain only white spaces.
 //return 1 for a token which is contain only alpha charecters.
-int i=0;
+int
+SplitToken (char **str, char *token)
+{
+  int i = 0;
 
-	if (**str==0)
-		return -1;
-	if (myisspace(**str)!=0){
-		//found white space.
-		while ( (myisspace(**str)!=0) && (**str!=0) ) {
-			*token=**str;
-			(*str)++;
-			token++;
-		}
-		*token=0;
-		return 0;
-	}else{
-		while ( (myisspace(**str)==0) && (**str!=0) ) {
-			*token=**str;
-			(*str)++;
-			token++;
-		}
-		*token=0;				
-		return 1;
-	}
+  if (**str == 0)
+    return -1;
+  if (myisspace (**str) != 0)
+    {
+      //found white space.
+      while (myisspace (**str) != 0 && **str != 0)
+        {
+          *token = **str;
+          (*str)++;
+          token++;
+        }
+      *token = 0;
+      return 0;
+    }
+  else
+    {
+      while (myisspace (**str) == 0 && **str != 0)
+        {
+          *token = **str;
+          (*str)++;
+          token++;
+        }
+      *token = 0;
+      return 1;
+    }
 }
 
-int myisspace(int ch){
-	
-	if ((ch>=9 && ch <=13) || (ch==32))
-		return ch;
-	else
-		return 0;
+int
+myisspace (int ch)
+{
 
+  if (0x09 <= ch && ch <= 0x0d || ch == ' ')
+    return ch;
+  else
+    return 0;
 }
 
-bool IsJunkChar(unsigned char ch){
-	if ((ch==9)||(ch==10)||(ch==13)||(ch >=32 && ch<=126)||(ch>=161 && ch<=251))
-		return false;
-	else
-		return true;
+bool
+IsJunkChar (unsigned char ch)
+{
+  if (ch == '\t' || ch == '\n' || ch == '\r' || (0x20 <= ch && ch <= 0x7e)
+      || (0xa1 <= ch && ch <= 0xfb))
+    return false;
+  else
+    return true;
 }
 
-void RemoveJunkChar(char *str){
-char *srcStr,*desStr;
-	srcStr=str;
-	desStr=str;
-	while (*srcStr!=0) {
-		if (!IsJunkChar(*srcStr)){
-			*desStr=*srcStr;
-			desStr++;
-		}
-		srcStr++;
-	}
-	*desStr=0;
+void
+RemoveJunkChar (char *str)
+{
+  char *srcStr, *desStr;
+  srcStr = str;
+  desStr = str;
+  while (*srcStr != 0)
+    {
+      if (!IsJunkChar (*srcStr))
+        {
+          *desStr = *srcStr;
+          desStr++;
+        }
+      srcStr++;
+    }
+  *desStr = 0;
 }
