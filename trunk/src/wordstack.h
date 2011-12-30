61c06e2ddef54073d7ee06d4cc650745d1cc5120
thep-svn <thep-svn@5165837f-0e3e-446d-8197-18d607f76d43>
2011-12-30 14:46:28 +0000

    * src/wordstack.h: 	  - Apply "indent -nut" + manual adjustments.

--- trunk/src/wordstack.h
+++ trunk/src/wordstack.h
@@ -2,44 +2,57 @@
 
 #define STACKSIZE 500
 
-class WordStack {
+class WordStack
+{
 private:
-	wordState stack[STACKSIZE];
-	short int top_idx;
+  wordState stack[STACKSIZE];
+  short int top_idx;
+
 public:
-	WordStack();
-	bool Push(wordState w);
-	bool Pop();
-	wordState Top();
-	bool Empty();
+  WordStack ();
+
+  bool Push (wordState w);
+  bool Pop ();
+  wordState Top ();
+  bool Empty ();
 };
 
-inline WordStack::WordStack() : top_idx (-1) {}
-
-inline bool WordStack::Push (wordState w)
+inline
+WordStack::WordStack ()
+  : top_idx (-1)
 {
-	if (top_idx < STACKSIZE-1){
-		stack[++top_idx] = w;
-		return true;
-	}
-	return false;
 }
 
-inline bool WordStack::Pop()
+inline bool
+WordStack::Push (wordState w)
 {
-	if (top_idx >= 0) {
-		top_idx--;
-		return true;
-	}
-	return false;
+  if (top_idx < STACKSIZE - 1)
+    {
+      stack[++top_idx] = w;
+      return true;
+    }
+  return false;
 }
 
-inline bool WordStack::Empty()
+inline bool
+WordStack::Pop ()
 {
-	return (top_idx < 0);
+  if (top_idx >= 0)
+    {
+      top_idx--;
+      return true;
+    }
+  return false;
 }
 
-inline wordState WordStack::Top()
+inline bool
+WordStack::Empty ()
 {
-	return (top_idx >= 0) ? stack[top_idx] : wordState (-1, 0);
+  return top_idx < 0;
+}
+
+inline wordState
+WordStack::Top ()
+{
+  return (top_idx >= 0) ? stack[top_idx] : wordState (-1, 0);
 }
