db62484815b15804bac7e407f94012ea3d3e6c04
thep-svn <theppitak@gmail.com>
2011-12-30 08:56:57 +0000

    * src/abswordseg.h, src/abswordseg.cpp: 	  - Reformat source as GNU style using "indent -nut" plus manual 	    adjustments.

--- src/abswordseg.cpp
+++ src/abswordseg.cpp
@@ -14,245 +14,302 @@
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
-AbsWordSeg::AbsWordSeg(){
-	IdxSep = new short int [MAXLEN];
-	LinkSep = new short int [3*MAXLEN];
-	if ((IdxSep==NULL)||(LinkSep==NULL)){
-		fprintf(stderr, "Cannot allocate memory\n");
-		exit(0);
-	}
-	MyDict = NULL;
-}
-
-AbsWordSeg::~AbsWordSeg(){
-	delete[] IdxSep;
-	delete[] LinkSep;
-
-	if (MyDict) {
-		trie_free(MyDict);
-	}
-}
-
-bool AbsWordSeg::InitDict(const char *dictPath)
+AbsWordSeg::AbsWordSeg ()
 {
-	char* trieBuff = NULL;
-	const char* triePath;
-	struct stat sb;
-
-	if (stat (dictPath, &sb) == -1) {
-		perror (dictPath);
-		return false;
-	}
-
-	if (S_ISDIR (sb.st_mode)) {
-		trieBuff = new char[strlen(dictPath)+2+sizeof(D2TRIE)];
-		sprintf (trieBuff, "%s" PATHSEPERATOR D2TRIE, dictPath);
-		triePath = trieBuff;
-	} else if (S_ISREG (sb.st_mode)) {
-		triePath = dictPath;
-	} else {
-		fprintf (stderr, "%s is not a directory or regular file\n",
-		         dictPath);
-		return false;
-	}
-
-	MyDict = trie_new_from_file (triePath);
-
-	if (trieBuff) {
-		delete[] trieBuff;
-	}
-
-	return MyDict != NULL;
-}
-
-
-#define tis2uni(c)  ((c)&0x80?((c)-0xa0+0x0e00):(c))
-#define isthaidigit(c) (0xf0<=(c)&&(c)<=0xf9)
-
-void AbsWordSeg::CreateWordList(void){
-char Buff[2000];
-short int i,j,cntLink,en_word;
-short int cntFound,amb_sep_cnt;
-int data_idx;
-unsigned char lead_ch;
-TrieState *curState;
-
-   Buff[0]='\0';
-   cntLink=0;
-   amb_sep_cnt=0;
-   curState=trie_root(MyDict);
-   for(i=0;i<len;i++) { //word boundry start at i and end at j.
-      if ( !IsLeadChar((unsigned char)sen[i])
-           || (i > 0 && !IsLastChar((unsigned char)sen[i-1])) )
-      {
-        IdxSep[i]=-2; //cannot leading for unknown word.
-        continue;
-      }
-      lead_ch=(unsigned char)sen[i];
-      // FIND STRING OF PUNCTUATION.
-      if (ispunct (lead_ch)) {
-        IdxSep[i]=cntLink;
-        do {
-                lead_ch = (unsigned char)sen[++i];
-        } while (lead_ch != '\0' && ispunct (lead_ch));
-        LinkSep[cntLink++]=i;
-        LinkSep[cntLink++]=-1;
-        i--;
-        continue;
-      }
-      // FIND STRING OF NUMBER.
-      if (isdigit (lead_ch) || isthaidigit (lead_ch)) {
-        IdxSep[i]=cntLink;
-        do {
-                lead_ch = (unsigned char)sen[++i];
-        } while (lead_ch != '\0' && isdigit (lead_ch) || isthaidigit (lead_ch)
-         	 || lead_ch=='.' || lead_ch==',');
-        LinkSep[cntLink++]=i;
-        LinkSep[cntLink++]=-1;
-        i--;
-       	continue;
-      }
-      // FIND STRING OF ENGLISH.
-      if (isalpha (lead_ch)){
-        IdxSep[i]=cntLink;
-        do {
-                lead_ch = (unsigned char)sen[++i];
-        } while (lead_ch != '\0' && isalpha (lead_ch));
-        LinkSep[cntLink++]=i;
-        LinkSep[cntLink++]=-1;
-        i--;
-        continue;
-      }
-      cntFound=0;
-      trie_state_rewind(curState);
-      for (j = 0; i+j<len; j++) {
-	 if ((sen[i+j]==0xe6 )&&(cntFound!=0)) {//Mai-Ya-Mok -- Stop word point 17 July 2001
-		LinkSep[cntLink-1]=i+j+1;
-		break;
-	 }
-         if (!trie_state_walk(curState,tis2uni((unsigned char)sen[i+j])))
-	 	break;
-	 if( trie_state_is_terminal(curState) ) {
-		//found word in dictionary
-		//check whether it should be segmented here
-		if ( IsLeadChar((unsigned char)sen[i+j+1])
-		     && !Has_Karun(&sen[i+j],&en_word) )
-		{
-			LinkSep[cntLink]=i+j+1;
-			LinkSep[cntLink+1]=-1; //debug 24 nov
-			//LinkSepDataIdx[cntLink]=data_idx;
-			cntFound++;
-			if (cntFound==1) IdxSep[i]=cntLink;
-			cntLink++;
-		}
-         }
-      }
-      if (cntFound==0)
-      	IdxSep[i]=-1; //
-      else if (cntFound < 2000){
-        //LinkSepDataIdx[cntLink]=-1;
-      	LinkSep[cntLink++]=-1;
-      }
-   } //end for i
-   trie_state_free(curState);
-//   amb_sep[amb_sep_cnt].st_idx=-1;
-	LinkSep[cntLink]=-1; 
-	LinkSep[++cntLink]=-1; //add stop value;
-}
-
-bool AbsWordSeg::IsNumber(const char *str)
-{
-    while (*str) {
-        if (!isdigit(*str) && *str != '.' && *str != ',')
-            return false;
-        str++;
+  IdxSep = new short int[MAXLEN];
+  LinkSep = new short int[3 * MAXLEN];
+  if (IdxSep == NULL || LinkSep == NULL)
+    {
+      fprintf (stderr, "Cannot allocate memory\n");
+      exit (0);
     }
-    return true;
+  MyDict = NULL;
 }
-bool AbsWordSeg::IsEnglish(const char *str)
+
+AbsWordSeg::~AbsWordSeg ()
 {
-    while (*str) {
-        if (!isalpha(*str) && (*str != '.') && (*str != '-') && (*str != ' '))
+  delete[] IdxSep;
+  delete[] LinkSep;
+
+  if (MyDict)
+    {
+      trie_free (MyDict);
+    }
+}
+
+bool
+AbsWordSeg::InitDict (const char *dictPath)
+{
+  char *trieBuff = NULL;
+  const char *triePath;
+  struct stat sb;
+
+  if (stat (dictPath, &sb) == -1)
+    {
+      perror (dictPath);
+      return false;
+    }
+
+  if (S_ISDIR (sb.st_mode))
+    {
+      trieBuff = new char[strlen (dictPath) + 2 + sizeof (D2TRIE)];
+      sprintf (trieBuff, "%s" PATHSEPERATOR D2TRIE, dictPath);
+      triePath = trieBuff;
+    }
+  else if (S_ISREG (sb.st_mode))
+    {
+      triePath = dictPath;
+    }
+  else
+    {
+      fprintf (stderr, "%s is not a directory or regular file\n", dictPath);
+      return false;
+    }
+
+  MyDict = trie_new_from_file (triePath);
+
+  if (trieBuff)
+    {
+      delete[] trieBuff;
+    }
+
+  return MyDict != NULL;
+}
+
+
+#define tis2uni(c)  ((c) & 0x80 ? (c) - 0xa0 + 0x0e00 : (c))
+#define isthaidigit(c) (0xf0 <= (c) && (c) <= 0xf9)
+
+void
+AbsWordSeg::CreateWordList (void)
+{
+  char Buff[2000];
+  short int i, j, cntLink, en_word;
+  short int cntFound, amb_sep_cnt;
+  int data_idx;
+  unsigned char lead_ch;
+  TrieState *curState;
+
+  Buff[0] = '\0';
+  cntLink = 0;
+  amb_sep_cnt = 0;
+  curState = trie_root (MyDict);
+  for (i = 0; i < len; i++)
+    {                                //word boundry start at i and end at j.
+      if (!IsLeadChar ((unsigned char) sen[i])
+          || (i > 0 && !IsLastChar ((unsigned char) sen[i - 1])))
         {
-            return false;
+          IdxSep[i] = -2;        //cannot leading for unknown word.
+          continue;
         }
-        str++;
+      lead_ch = (unsigned char) sen[i];
+      // FIND STRING OF PUNCTUATION.
+      if (ispunct (lead_ch))
+        {
+          IdxSep[i] = cntLink;
+          do
+            {
+              lead_ch = (unsigned char) sen[++i];
+            }
+          while (lead_ch != '\0' && ispunct (lead_ch));
+          LinkSep[cntLink++] = i;
+          LinkSep[cntLink++] = -1;
+          i--;
+          continue;
+        }
+      // FIND STRING OF NUMBER.
+      if (isdigit (lead_ch) || isthaidigit (lead_ch))
+        {
+          IdxSep[i] = cntLink;
+          do
+            {
+              lead_ch = (unsigned char) sen[++i];
+            }
+          while (lead_ch != '\0' && isdigit (lead_ch) || isthaidigit (lead_ch)
+                 || lead_ch == '.' || lead_ch == ',');
+          LinkSep[cntLink++] = i;
+          LinkSep[cntLink++] = -1;
+          i--;
+          continue;
+        }
+      // FIND STRING OF ENGLISH.
+      if (isalpha (lead_ch))
+        {
+          IdxSep[i] = cntLink;
+          do
+            {
+              lead_ch = (unsigned char) sen[++i];
+            }
+          while (lead_ch != '\0' && isalpha (lead_ch));
+          LinkSep[cntLink++] = i;
+          LinkSep[cntLink++] = -1;
+          i--;
+          continue;
+        }
+      cntFound = 0;
+      trie_state_rewind (curState);
+      for (j = 0; i + j < len; j++)
+        {
+          if (sen[i + j] == 0xe6 && cntFound != 0)
+            {                        //Mai-Ya-Mok -- break position
+              LinkSep[cntLink - 1] = i + j + 1;
+              break;
+            }
+          if (!trie_state_walk (curState, tis2uni ((unsigned char) sen[i + j])))
+            break;
+          if (trie_state_is_terminal (curState))
+            {
+              //found word in dictionary
+              //check whether it should be segmented here
+              if (IsLeadChar ((unsigned char) sen[i + j + 1])
+                  && !Has_Karun (&sen[i + j], &en_word))
+                {
+                  LinkSep[cntLink] = i + j + 1;
+                  LinkSep[cntLink + 1] = -1;
+                  //LinkSepDataIdx[cntLink]=data_idx;
+                  cntFound++;
+                  if (cntFound == 1)
+                    IdxSep[i] = cntLink;
+                  cntLink++;
+                }
+            }
+        }
+      if (cntFound == 0)
+        IdxSep[i] = -1;
+      else if (cntFound < 2000)
+        {
+          //LinkSepDataIdx[cntLink]=-1;
+          LinkSep[cntLink++] = -1;
+        }
     }
-    return true;
+  trie_state_free (curState);
+//   amb_sep[amb_sep_cnt].st_idx=-1;
+  LinkSep[cntLink] = -1;
+  LinkSep[++cntLink] = -1;        //add stop value
 }
 
-bool AbsWordSeg::IsLeadChar(unsigned char ch){
-	return ! ((0xcf<=ch && ch<=0xda) || (0xe5<=ch && ch<=0xee));
-}
-bool AbsWordSeg::IsLastChar(unsigned char ch){
-	return ! ((0xe0<=ch && ch<=0xe4) || ch==0xd1);
-}
-bool AbsWordSeg::Has_Karun(const char* sen_ptr,short int *k_idx){
-short int i;
-   *k_idx=-1;
-   for (i=1;i<=3;i++){
-      if (sen_ptr[i]=='\0')
-         break;
-      if ((unsigned char)sen_ptr[i]==0xec){
-         *k_idx=i;
-         return true;
-      }
-   }
-   return false;
+bool
+AbsWordSeg::IsNumber (const char *str)
+{
+  while (*str)
+    {
+      if (!isdigit (*str) && *str != '.' && *str != ',')
+        return false;
+      str++;
+    }
+  return true;
 }
 
-bool AbsWordSeg::WordSeg(char* senstr,char *output,const char *wbr){
-int bestidx;
-   strcpy(sen,senstr);
-   len=strlen(senstr);
-   InitData();
-   CreateWordList();
-   SwapLinkSep();
-   bestidx=CreateSentence();
-   GetBestSen(bestidx,wbr,output);
-   
-   return 0;
+bool
+AbsWordSeg::IsEnglish (const char *str)
+{
+  while (*str)
+    {
+      if (!isalpha (*str) && *str != '.' && *str != '-' && *str != ' ')
+        return false;
+      str++;
+    }
+  return true;
 }
 
-void AbsWordSeg::SwapLinkSep(){
-short int st_idx,en_idx,end_point,tmp;
-
-	st_idx=0; en_idx=0;
-	while (LinkSep[st_idx]!=-1) {
-		while (LinkSep[en_idx++]!=-1);
-        	end_point=en_idx-1;
-		en_idx-=2;
-        	while (st_idx<en_idx) {
-			tmp=LinkSep[st_idx];
-			//tmpI=LinkSepDataIdx[st_idx];
-			LinkSep[st_idx]=LinkSep[en_idx];
-			//LinkSepDataIdx[st_idx]=LinkSepDataIdx[en_idx];
-			st_idx++;
-			LinkSep[en_idx]=tmp;
-			//LinkSepDataIdx[en_idx]=tmpI;
-			en_idx--;
-		}
-		st_idx=end_point+1;
-		en_idx=st_idx;
-	}
+bool
+AbsWordSeg::IsLeadChar (unsigned char ch)
+{
+  return (ch < 0xcf || 0xda < ch) && (ch < 0xe5 || 0xee < ch);
 }
 
-void AbsWordSeg::GetWord(short int idxsen,short int idx,char *buff){
-char *sen_ptr;
-   sen_ptr=sen;
-	if (idx > 0) {
-		strncpy(buff,sen_ptr+SepData[idxsen].Sep[idx-1],SepData[idxsen].Sep[idx]-SepData[idxsen].Sep[idx-1]);
-   	buff[SepData[idxsen].Sep[idx]-SepData[idxsen].Sep[idx-1]]='\0';
-   }else{
-   	strncpy(buff,sen_ptr,SepData[idxsen].Sep[0]);
-      buff[SepData[idxsen].Sep[0]]='\0';
-   }
+bool
+AbsWordSeg::IsLastChar (unsigned char ch)
+{
+  return (ch < 0xe0 || 0xe4 < ch) && ch != 0xd1;
 }
 
-void AbsWordSeg::InitData(){
-int i;
-    for(i=0;(i<len) && (i<MAXLEN) ;i++){
-    	IdxSep[i]=-1;
+bool
+AbsWordSeg::Has_Karun (const char *sen_ptr, short int *k_idx)
+{
+  short int i;
+  *k_idx = -1;
+  for (i = 1; i <= 3 && sen_ptr[i] != '\0'; i++)
+    {
+      if ((unsigned char) sen_ptr[i] == 0xec)
+        {
+          *k_idx = i;
+          return true;
+        }
+    }
+  return false;
+}
+
+bool
+AbsWordSeg::WordSeg (char *senstr, char *output, const char *wbr)
+{
+  int bestidx;
+
+  strcpy (sen, senstr);
+  len = strlen (senstr);
+  InitData ();
+  CreateWordList ();
+  SwapLinkSep ();
+  bestidx = CreateSentence ();
+  GetBestSen (bestidx, wbr, output);
+
+  return 0;
+}
+
+void
+AbsWordSeg::SwapLinkSep ()
+{
+  short int st_idx, en_idx, end_point, tmp;
+
+  st_idx = 0;
+  en_idx = 0;
+  while (LinkSep[st_idx] != -1)
+    {
+      while (LinkSep[en_idx++] != -1)
+        ;
+      end_point = en_idx - 1;
+      en_idx -= 2;
+      while (st_idx < en_idx)
+        {
+          tmp = LinkSep[st_idx];
+          //tmpI=LinkSepDataIdx[st_idx];
+          LinkSep[st_idx] = LinkSep[en_idx];
+          //LinkSepDataIdx[st_idx]=LinkSepDataIdx[en_idx];
+          st_idx++;
+          LinkSep[en_idx] = tmp;
+          //LinkSepDataIdx[en_idx]=tmpI;
+          en_idx--;
+        }
+      st_idx = end_point + 1;
+      en_idx = st_idx;
+    }
+}
+
+void
+AbsWordSeg::GetWord (short int idxsen, short int idx, char *buff)
+{
+  char *sen_ptr;
+  sen_ptr = sen;
+  if (idx > 0)
+    {
+      strncpy (buff, sen_ptr + SepData[idxsen].Sep[idx - 1],
+               SepData[idxsen].Sep[idx] - SepData[idxsen].Sep[idx - 1]);
+      buff[SepData[idxsen].Sep[idx] - SepData[idxsen].Sep[idx - 1]] = '\0';
+    }
+  else
+    {
+      strncpy (buff, sen_ptr, SepData[idxsen].Sep[0]);
+      buff[SepData[idxsen].Sep[0]] = '\0';
+    }
+}
+
+void
+AbsWordSeg::InitData ()
+{
+  int i;
+  for (i = 0; i < len && i < MAXLEN; i++)
+    {
+      IdxSep[i] = -1;
     }
 
 }
@@ -263,36 +320,46 @@ int i;
 // has value= sepPoint).
 // Return index of next seperation point that will be fill.
 // ========================================================
-unsigned short int AbsWordSeg::copySepData(short int sourceIdxSen,short int targetIdxSen,short int sepPoint){
-short int i=0;
-	if (sourceIdxSen==targetIdxSen) {
-		while(SepData[sourceIdxSen].Sep[i]<=sepPoint){
-			i++;
-			if (SepData[sourceIdxSen].Sep[i-1]==sepPoint)
-				break;
-		}
-	}else {
-		while (SepData[sourceIdxSen].Sep[i]<=sepPoint){
-			SepData[targetIdxSen].Sep[i]=SepData[sourceIdxSen].Sep[i];
-			i++;
-			if (SepData[sourceIdxSen].Sep[i-1]==sepPoint)
-				break;
-		}
-	}
-	return i;
+unsigned short int
+AbsWordSeg::copySepData (short int sourceIdxSen, short int targetIdxSen,
+                         short int sepPoint)
+{
+  short int i = 0;
+  if (sourceIdxSen == targetIdxSen)
+    {
+      while (SepData[sourceIdxSen].Sep[i] <= sepPoint)
+        {
+          i++;
+          if (SepData[sourceIdxSen].Sep[i - 1] == sepPoint)
+            break;
+        }
+    }
+  else
+    {
+      while (SepData[sourceIdxSen].Sep[i] <= sepPoint)
+        {
+          SepData[targetIdxSen].Sep[i] = SepData[sourceIdxSen].Sep[i];
+          i++;
+          if (SepData[sourceIdxSen].Sep[i - 1] == sepPoint)
+            break;
+        }
+    }
+  return i;
 }
 
-void AbsWordSeg::GetBestSen(int bestidx,const char *wbr,char *outstr){
-int t;
-char buff[2000];
+void
+AbsWordSeg::GetBestSen (int bestidx, const char *wbr, char *outstr)
+{
+  int t;
+  char buff[2000];
 
-	*outstr='\0';
-	for (t=0;SepData[bestidx].Sep[t]!=len;t++) {
-		GetWord(bestidx,t,buff);
-		strcat(outstr,buff);
-		strcat(outstr,wbr);
-	}
-	GetWord(bestidx,t,buff);
-	strcat(outstr,buff);
+  *outstr = '\0';
+  for (t = 0; SepData[bestidx].Sep[t] != len; t++)
+    {
+      GetWord (bestidx, t, buff);
+      strcat (outstr, buff);
+      strcat (outstr, wbr);
+    }
+  GetWord (bestidx, t, buff);
+  strcat (outstr, buff);
 }
-
--- src/abswordseg.h
+++ src/abswordseg.h
@@ -12,41 +12,43 @@
 #include <datrie/trie.h>
 #include "worddef.h"
 
-class AbsWordSeg  
+class AbsWordSeg
 {
 public:
-	AbsWordSeg();
-	virtual ~AbsWordSeg();
-	bool InitDict(const char *dictPath);
-	bool WordSeg(char* senstr,char *output,const char *wbr);
-	
+  AbsWordSeg ();
+  virtual ~AbsWordSeg ();
+  bool InitDict (const char *dictPath);
+  bool WordSeg (char *senstr, char *output, const char *wbr);
+
 protected:
-	SepType SepData[3];
-	unsigned short int len;
-	char sen[MAXLEN];
-	short int *LinkSep;
-	short int *IdxSep;
-	//============Function for Wordseg=====================
-	virtual void SwapLinkSep();
-	virtual void GetBestSen(int bestidx,const char *wbr,char *outstr);
-	virtual void CreateWordList(void);
-	virtual int CreateSentence()=0; //return idx of best sen
-	virtual unsigned short int copySepData(short int sourceIdxSen,short int targetIdxSen,short int sepPoint);
-	void GetWord(short int idxsen,short int idx,char *buff);
+  SepType SepData[3];
+  unsigned short int len;
+  char sen[MAXLEN];
+  short int *LinkSep;
+  short int *IdxSep;
+  //============Function for Wordseg=====================
+  virtual void SwapLinkSep ();
+  virtual void GetBestSen (int bestidx, const char *wbr, char *outstr);
+  virtual void CreateWordList (void);
+  virtual int CreateSentence () = 0;    //return idx of best sen
+  virtual unsigned short int copySepData (short int sourceIdxSen,
+                                          short int targetIdxSen,
+                                          short int sepPoint);
+  void GetWord (short int idxsen, short int idx, char *buff);
 
 private:
-	void InitData();
+  void InitData ();
 
-	//============Check Character Type Function============
-	static bool IsLeadChar(unsigned char ch);
-	static bool IsLastChar(unsigned char ch);
-	static bool IsNumber(const char *str);
-	static bool IsEnglish(const char *str);
-	//============Check Karan rule=========================
-	static bool Has_Karun(const char* sen_ptr,short int *k_idx);
+  //============Check Character Type Function============
+  static bool IsLeadChar (unsigned char ch);
+  static bool IsLastChar (unsigned char ch);
+  static bool IsNumber (const char *str);
+  static bool IsEnglish (const char *str);
+  //============Check Karan rule=========================
+  static bool Has_Karun (const char *sen_ptr, short int *k_idx);
 
 private:
-	Trie *MyDict;
+    Trie * MyDict;
 };
 
 #endif // !defined(AFX_ABSWORDSEG_H__A0F72574_2912_11D3_B459_00105A5C2417__INCLUDED_)
