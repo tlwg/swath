e4abc57f1c69a64d7cfdb3f530e4f8e3d4f06cf3
thep-svn <thep-svn@5165837f-0e3e-446d-8197-18d607f76d43>
2011-12-31 03:43:44 +0000

    * src/filterlatex.h, src/filterlatex.cpp: 	  - Apply "indent -nut" + manual adjustments.

--- trunk/src/filterlatex.h
+++ trunk/src/filterlatex.h
@@ -2,33 +2,37 @@
 //
 //////////////////////////////////////////////////////////////////////
 
-#if !defined(INC_FILTERLATEX)
-#define INC_FILTERLATEX
+#ifndef __FILTERLATEX_H
+#define __FILTERLATEX_H
 
 #if _MSC_VER >= 1000
 #pragma once
-#endif // _MSC_VER >= 1000
+#endif
 
 #include "filterx.h"
 
 
-class FilterLatex : public FilterX  
+class FilterLatex : public FilterX
 {
 public:
-	void Print(char *token,bool thaiFlag);
-	bool GetNextToken(char *token, bool *thaiFlag);
-	FilterLatex(FILE *filein, FILE *fileout, int latexflag=0);
-	virtual ~FilterLatex();
+  FilterLatex (FILE * filein, FILE * fileout, int latexflag = 0);
+  virtual ~ FilterLatex ();
+
+  void Print (char *token, bool thaiFlag);
+  bool GetNextToken (char *token, bool * thaiFlag);
 
 protected:
-	bool winCharSet; //==true for Windows char set : false for Macintosh char set
-	int latexFlag; // 0 do nothing 1 win char set 2 mac char set
-	static int idxVowelToneMark(unsigned char ch);
-	static bool isLongTailChar(unsigned char ch);
-	void AdjustText (unsigned char *input, unsigned char *output);
+  bool winCharSet;  // true for Windows char set; false for Mac char set
+  int latexFlag;    // 0 do nothing; 1 win char set; 2 mac char set
+
+  static int idxVowelToneMark (unsigned char ch);
+  static bool isLongTailChar (unsigned char ch);
+
+  void AdjustText (unsigned char *input, unsigned char *output);
+
 private:
-	char buffer[2000];
-	bool verbatim;
+  char buffer[2000];
+  bool verbatim;
 };
 
-#endif // !defined(AFX_FILTERLATEX_H__6AB8F3C5_2230_11D3_B44A_00105A5C2417__INCLUDED_)
+#endif
