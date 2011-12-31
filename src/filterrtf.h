1a1bbf82ab2cb434fad1c53dad8f3908d988423c
thep-svn <theppitak@gmail.com>
2011-12-31 04:40:20 +0000

    * src/filterrtf.h, src/filterrtf.cpp: 	  - Apply "indent -nut" + manual adjustments.

--- src/filterrtf.h
+++ src/filterrtf.h
@@ -2,31 +2,33 @@
 //
 //////////////////////////////////////////////////////////////////////
 
-#if !defined(AFX_FILTERRTF_H__5FA01278_2213_11D3_B449_00105A5C2417__INCLUDED_)
-#define AFX_FILTERRTF_H__5FA01278_2213_11D3_B449_00105A5C2417__INCLUDED_
+#ifndef __FILTERRTF_H
+#define __FILTERRTF_H
 
 #if _MSC_VER >= 1000
 #pragma once
-#endif // _MSC_VER >= 1000
+#endif
 
 #include "filterx.h"
 
-class FilterRTF : public FilterX  
+class FilterRTF : public FilterX
 {
 public:
-	void Print(char *token,bool thaiFlag);
-	bool GetNextToken(char *token, bool *thaiFlag);
-	FilterRTF(FILE *filein, FILE *fileout);
-	virtual ~FilterRTF();
+  FilterRTF (FILE* filein, FILE* fileout);
+  virtual ~FilterRTF ();
+
+  void Print (char* token, bool thaiFlag);
+  bool GetNextToken (char* token, bool* thaiFlag);
 
 private:
-	int psState;
-	char strbuff[5];
-	//sequence of characters is    \ ' x x (one character)
-	//states of this sequence are  1 2 3 4 	
-	static int chgCharState(char charin, int state);
-	static bool isThaiChar(char *token, int *state);
+  int psState;
+  char strbuff[5];
 
+private:
+  //sequence of characters is    \ ' x x (one character)
+  //states of this sequence are  1 2 3 4  
+  static int chgCharState (char charin, int state);
+  static bool isThaiChar (char *token, int *state);
 };
 
-#endif // !defined(AFX_FILTERRTF_H__5FA01278_2213_11D3_B449_00105A5C2417__INCLUDED_)
+#endif
