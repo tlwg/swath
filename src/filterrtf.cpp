91192b090b0a32fa377027d0f4ca10b02da5b61f
thep-svn <theppitak@gmail.com>
2012-06-22 10:19:07 +0000

    Escape RTF word break.
    	* src/filterrtf.cpp (FilterRTF::FilterRTF):
    	  - Escape word break text, according to Widhaya Trisarnwadhana.

--- src/filterrtf.cpp
+++ src/filterrtf.cpp
@@ -11,7 +11,7 @@
 
 FilterRTF::FilterRTF (FILE* filein, FILE* fileout, bool isUniIn, bool isUniOut)
   : FilterX (filein, fileout, isUniIn, isUniOut,
-    isUniOut ? "\E2\x80\x8B" : "\xDC"), // U+200B or \xDC
+    isUniOut ? "\\u8203\\'e2\\'80\\'8b" : "\\'dc"), // U+200B or \xDC
     psState (0)
 {
   strbuff[0] = '\0';
