769aed66a7a445b4a9a642922bfdffcbe050eafd
Theppitak Karoonboonyanan <theppitak@gmail.com>
2016-07-06 14:54:55 +0700

    Add test suite, with simple test case.
    * configure.ac, Makefile.am:
      - Add tests/ subdir.
    
    * +tests/Makefile.am, +tests/test-simple.sh:
      - Add a simple test case.

---
+++ tests/test-simple.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+test_wseg()
+{
+  INPUT=$1
+  EXPECT=$2
+
+  RES=$(echo -n "${INPUT}" | ${SWATH} -d ${DICTDIR} -u u,u)
+
+  if test "x${RES}" != "x${EXPECT}"; then
+    return -1
+  fi
+
+  return 0
+}
+
+INPUT="ทดสอบการตัดคำอย่างง่าย"
+EXPECT="ทดสอบ|การ|ตัด|คำ|อย่าง|ง่าย"
+
+test_wseg ${INPUT} ${EXPECT}
