a053864a85a0121bdf605c0fe4b3c64e45edc7e2
Theppitak Karoonboonyanan <theppitak@gmail.com>
2016-07-06 16:13:28 +0700

    Add test case for UTF-8 word break code.
    * tests/Makefile.am, +tests/test-utf8-wbr.sh:
      - Add test case for non-ASCII word break string,
        for Issue #1.

---
+++ tests/test-utf8-wbr.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#
+# Test of UTF-8 word delimiter code
+# Ref: https://github.com/tlwg/swath/issues/1
+#
+
+set -e -x
+
+INPUT="ผมมีแฟนแล้วนะครับ"
+EXPECT="ผม​มี​แฟน​แล้ว​นะ​ครับ"
+
+RES=$(echo -n "${INPUT}" | ${SWATH} -d ${DICTDIR} -u u,u -b "​")
+
+if test "x${RES}" != "x${EXPECT}"; then
+  exit 1
+fi
